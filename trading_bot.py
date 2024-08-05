5143a0e1998353b69e61a4a668e59d75ef06baaa
Aweinman <aminweinman@gmail.com>
2024-08-05 16:45:10 -0600

    Adding Alpaca API endpoints

--- trading_bot.py
+++ trading_bot.py
@@ -7,3 +7,14 @@ from alpaca_trade_api import REST
 from timedelta import Timedelta
 from finbert_utils import estimate_sentiment
 
+# Alpaca API credentials
+API_KEY = "PKS85182RLQJ94DBJE83"
+API_SECRET = "Tbdm4lNyjRpcCi8I2PUvDkGv1Z2ENhVyFXiWZrRf"
+BASE_URL = "https://paper-api.alpaca.markets/v2"
+
+ALPACA_CREDS = {
+    "API_KEY": API_KEY,
+    "API_SECRET": API_SECRET,
+    "PAPER": True
+}
+
