94f588bb7be742195bf1f27d8ae9fc6c57416d84
Aweinman <aminweinman@gmail.com>
2024-08-06 16:40:04 -0600

    adding date and sentiment analysis

--- trading_bot.py
+++ trading_bot.py
@@ -31,4 +31,18 @@ class MLTrader(Strategy):
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price, 0)
         return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+
+    def get_sentiment(self):
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol,
+                                 start=three_days_prior,
+                                 end=today)
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
 
