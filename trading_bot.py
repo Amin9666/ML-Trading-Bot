5dc8de917bdad918cc84c2d9cd48c44388418505
Aweinman <aminweinman@gmail.com>
2024-08-06 08:11:18 -0600

    Adding initialization and positioning

--- trading_bot.py
+++ trading_bot.py
@@ -18,3 +18,17 @@ ALPACA_CREDS = {
     "PAPER": True
 }
 
+class MLTrader(Strategy):
+    def initialize(self, symbol: str = "SPY", cash_at_risk: float = .5):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+        self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price, 0)
+        return cash, last_price, quantity
+
